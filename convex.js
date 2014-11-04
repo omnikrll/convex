d96c8aba60e30b574c0782bc97ac064d4c251b8d
Karl Hohn <karl.hohn@liveauctioneers.com>
2014-11-04 17:24:59 -0500

    began splitting out matrix into its own class

--- convex.js
+++ convex.js
@@ -9,108 +9,7 @@ var carrierMenu = document.getElementById('carrierMenu');
 var modulatorMenu = document.getElementById('modulatorMenu');
 
 //samples to be loaded
-var samples = [
-  {
-    'name': 'Boy Voice',
-    'path': '../convex/sounds/bruno.wav'
-  },
-  { 
-    'name': 'Fripp Guitar',
-    'path': '../convex/sounds/frippguitar.wav'
-  },
-  { 
-    'name': 'Teapot',
-    'path': '../convex/sounds/teapot.wav'
-  },
-  {
-    'name': 'Cello',
-    'path': '../convex/sounds/cello.wav'
-  },
-  {
-    'name': 'Tape Choir 1',
-    'path': '../convex/sounds/tape-choir.wav'
-  },
-  {
-    'name': 'Tape Choir 2',
-    'path': '../convex/sounds/tape-choir-2.wav'
-  },
-  {
-    'name': 'Cheapo Synth',
-    'path': '../convex/sounds/cheapo-synth.wav'
-  },
-  {
-    'name': 'Processed Image Wave',
-    'path': '../convex/sounds/andrews-img.wav'
-  },
-  {
-    'name': 'Tape Flute',
-    'path': '../convex/sounds/tape-flute.wav'
-  },
-  {
-    'name': 'Tape Female Voice',
-    'path': '../convex/sounds/tape-femvoice.wav'
-  },
-  {
-    'name': 'Brain Massage',
-    'path': '../convex/sounds/brain-massage.wav'
-  },
-  { 
-    'name': 'Dot Matrix Printer',
-    'path': '../convex/sounds/printer.wav'
-  },
-  { 
-    'name': 'Transporter Beam',
-    'path': '../convex/sounds/transporter.wav'
-  },
-  {
-    'name': 'Tricorder Glitch',
-    'path': '../convex/sounds/denybeep.wav'
-  },
-  {
-    'name': 'Van der Pol (Driven)',
-    'path': '../convex/sounds/vanderpoldriven.wav'
-  },
-  {
-    'name': 'Van der Pol (Sweep)',
-    'path': '../convex/sounds/vanderpolsweep.wav'
-  },
-  {
-    'name': 'Dishwasher',
-    'path': '../convex/sounds/dishwasher.wav'
-  },
-  {
-    'name': 'Circuit Bent Noise 1',
-    'path': '../convex/sounds/circuitbend-1.wav'
-  },
-  {
-    'name': 'Circuit Bent Noise 2',
-    'path': '../convex/sounds/circuitbend-2.wav'
-  },
-  {
-    'name': 'Circuit Bent Noise 3',
-    'path': '../convex/sounds/circuitbend-3.wav'
-  },
-  {
-    'name': 'Circuit Bent Noise 4',
-    'path': '../convex/sounds/circuitbend-4.wav'
-  },
-  {
-    'name': 'Ray Gun 1',
-    'path': '../convex/sounds/raygun1.wav'
-  },
-  {
-    'name': 'Ray Gun 2',
-    'path': '../convex/sounds/raygun2.wav'
-  },
-  {
-    'name': 'Ray Gun 3',
-    'path': '../convex/sounds/raygun3.wav'
-  },
-  {
-    'name': 'Ray Gun 4',
-    'path': '../convex/sounds/raygun4.wav'
-  }
-];
+
 
 // define uncasted matrix node names:
 var carrier; // bufferSource for sample playback
@@ -141,7 +40,7 @@ function init() {
       }
     });
   } catch(e) {
-    alert("Whoops! Something's wrong-- " + e.name + ": " + e.message + "\nAre you viewing this page in Chrome or Safari? Is your browser up to date?");
+    alert(e.name + "\n" + e.message +);
   }
 }
 
