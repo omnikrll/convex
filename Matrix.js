6b9f859d44545d5c45a1d90f3208e2defeaff430
Karl Hohn <karl.hohn@liveauctioneers.com>
2014-11-04 17:25:48 -0500

    whoops forgot to add files :/

---
+++ Matrix.js
@@ -0,0 +1,31 @@
+var Matrix = (function(context, buffers) {
+
+	var Matrix = function(context) {
+		this.context = context;
+		this.buffers = buffers;
+		this.voices = [];
+		this.sourceIndex = 0;
+		this.impulseIndex = 0;
+		this.semitoneRatio = Math.pow(2, 1/12); // multiplier for chromatic scaling
+		this.root = 69; // root key is A440 by default
+		this.rampMin = 0.00001; // ensure fine scaling for ramping functions
+		this.rampMax = 1 - rampMin;
+
+		this.init();
+	}
+
+	Matrix.prototype.constructor = Matrix;
+
+	Matrix.prototype.init = function() {
+		var self = this;
+
+		// init function
+	}
+
+	Matrix.prototype.bindEventHandlers = function() {
+		// bind dom handlers here
+	}
+
+	return Matrix;
+	
+})();
